3d539fcbc6f935135a8b00bf4f3dfff5103c985a
Tooba Munir <tooba.munir300@gmail.com>
2026-06-26 23:53:33 +0500

    removed comments

--- tests/todo.spec.js
+++ tests/todo.spec.js
@@ -15,26 +15,4 @@ test('To Do MVC React App Test', async ({ page }) => {
 {
  await todo.expectValueToBe(dataForTest.TODO_ITEMS[i],i);
 }
-
-
-  /* await page.getByTestId('text-input').press('CapsLock');
-  await page.getByTestId('text-input').fill('D');
-  await page.getByTestId('text-input').press('CapsLock');
-  await page.getByTestId('text-input').fill('Dishes');
-  await page.getByTestId('text-input').press('Enter');
-  await page.getByTestId('text-input').press('CapsLock');
-  await page.getByTestId('text-input').fill('W');
-  await page.getByTestId('text-input').press('CapsLock');
-  await page.getByTestId('text-input').fill('Work');
-  await page.getByTestId('text-input').press('Enter');
-  await page.getByTestId('text-input').press('CapsLock');
-  await page.getByTestId('text-input').fill('S');
-  await page.getByTestId('text-input').press('CapsLock');
-  await page.getByTestId('text-input').fill('Study');
-  await page.getByTestId('text-input').press('Enter');
-  await page.getByRole('listitem').filter({ hasText: 'Dishes' }).getByTestId('todo-item-toggle').check();
-  await page.getByRole('link', { name: 'Completed' }).click();
-  await expect(page.getByTestId('todo-item-label')).toBeVisible();
-  await page.getByRole('link', { name: 'Active' }).click();
-  await page.getByTestId('footer-navigation').click(); */
 });
